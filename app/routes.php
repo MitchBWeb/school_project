7f95c4ae58a767f6020de794d3cd787225f00d91
mitch <mitch@dijatek.com>
2015-03-08 11:52:59 -0600

    Added comments to routes file

--- app/routes.php
+++ app/routes.php
@@ -14,6 +14,8 @@
 // Home page will display the login form.
 Route::get('/', function()
 {
+    // If the user is already logged on there is no need for them to access
+    // the root file (home page). If they aren't logged in then they see it.
     if(Auth::check()){
         if(Auth::user()->role_id != 'admin'){
             return View::make('profile/index');
